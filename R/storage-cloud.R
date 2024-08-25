f213304d2f76402b7691db4f0037fab323368754
langbart <lorenzo.longobardi@gmail.com>
2024-08-25 20:32:05 +0200

    create mongodb pushing function

--- R/storage-cloud.R
+++ R/storage-cloud.R
@@ -13,7 +13,7 @@
 #' @examples
 #' \dontrun{
 #' # Retrieve data from a MongoDB collection
-#' result <- mongodb_get(
+#' result <- mdb_collection_pull(
 #'   connection_string = "mongodb://localhost:27017",
 #'   collection_name = "my_collection",
 #'   db_name = "my_database"
@@ -21,9 +21,53 @@
 #' }
 #'
 #' @export
-mdb_collection <- function(connection_string = NULL, collection_name = NULL, db_name = NULL) {
+mdb_collection_pull <- function(connection_string = NULL, collection_name = NULL, db_name = NULL) {
   data <- mongolite::mongo(collection = collection_name, db = db_name, url = connection_string)
   data$find()
+}
+
+#' Upload Data to MongoDB and Overwrite Existing Content
+#'
+#' This function connects to a MongoDB database, removes all existing documents
+#' from a specified collection, and then inserts new data.
+#'
+#' @param data A data frame containing the data to be uploaded.
+#' @param connection_string A character string specifying the MongoDB connection URL.
+#' @param collection_name A character string specifying the name of the collection.
+#' @param db_name A character string specifying the name of the database.
+#'
+#' @return The number of documents inserted.
+#'
+#' @importFrom mongolite mongo
+#'
+#' @examples
+#' \dontrun{
+#' # Upload and overwrite data in a MongoDB collection
+#' result <- mdb_collection_push(
+#'   data = processed_legacy_landings,
+#'   connection_string = "mongodb://localhost:27017",
+#'   collection_name = "my_collection",
+#'   db_name = "my_database"
+#' )
+#' }
+#'
+#' @export
+mdb_collection_push <- function(data = NULL, connection_string = NULL, collection_name = NULL, db_name = NULL) {
+  # Connect to the MongoDB collection
+  collection <- mongolite::mongo(
+    collection = collection_name,
+    db = db_name,
+    url = connection_string
+  )
+
+  # Remove all existing documents in the collection
+  collection$remove("{}")
+
+  # Insert the new data
+  result <- collection$insert(data)
+
+  # Return the number of documents inserted
+  return(result)
 }
 
 #' Get metadata tables
