64e2c976de9267ceeafea8c17af47634253f2d62
langbart <lorenzo.longobardi@gmail.com>
2024-08-26 17:51:23 +0200

    generate function to extract effort and cpue from validated legacy data

---
+++ R/export.R
@@ -0,0 +1,82 @@
+#' Export Summarized Fishery Data
+#'
+#' @description
+#' This function reshapes validated legacy fishery data, calculates summary metrics
+#' such as effort and CPUE (Catch Per Unit Effort), and uploads the processed data
+#' to MongoDB for dashboard usage.
+#'
+#' @details
+#' The function performs the following operations:
+#' 1. Pulls validated data from the "legacy-validated" MongoDB collection.
+#' 2. Reshapes the data by grouping by survey_id and summarizing catch and price.
+#' 3. Joins with BMU (Beach Management Unit) size data.
+#' 4. Calculates effort and CPUE.
+#' 5. Selects relevant columns for the final dataset.
+#' 6. Uploads the processed data to a MongoDB collection.
+#'
+#' Effort and CPUE are calculated as follows:
+#' - Effort = Number of fishers / Size of BMU in km
+#' - CPUE = Total catch in kg / Effort
+#'
+#' @return
+#' This function does not return a value. It processes the data and uploads
+#' the result to MongoDB.
+#'
+#' @note
+#' This function requires a configuration file to be present and readable by the
+#' 'read_config' function, which should provide MongoDB connection details.
+#' It also assumes the existence of a 'bmu_size' dataset for joining.
+#'
+#' @examples
+#' \dontrun{
+#' export_summaries()
+#' }
+#'
+#' @export
+export_summaries <- function() {
+  conf <- read_config()
+
+  valid_data <-
+    mdb_collection_pull(
+      connection_string = conf$storage$mongodb$connection_string,
+      collection_name = conf$storage$mongod$database$pipeline$collection_name$legacy$validated,
+      db_name = conf$storage$mongod$database$pipeline$name
+    ) |>
+    dplyr::as_tibble()
+
+  bmu_size <-
+    get_metadata()$fishing_grounds |>
+    dplyr::mutate(size_km = as.numeric(.data$size_km))
+
+  summaries <-
+    valid_data |>
+    dplyr::rename(BMU = .data$landing_site) |>
+    dplyr::group_by(.data$survey_id) |>
+    dplyr::summarise(dplyr::across(c(.data$BMU:.data$ecology), ~ dplyr::first(.x)),
+      catch_kg = sum(.data$catch_kg, na.rm = T),
+      price = mean(.data$price, na.rm = T)
+    ) |>
+    dplyr::ungroup() |>
+    dplyr::left_join(bmu_size, by = "BMU") |>
+    dplyr::rowwise() |>
+    dplyr::mutate(
+      effort = .data$no_of_fishers / .data$size_km,
+      cpue = .data$catch_kg / .data$effort
+    ) |>
+    dplyr::ungroup() |>
+    dplyr::select(
+      "survey_id", "BMU", "landing_date",
+      "gear", "catch_kg",
+      "price", "effort", "cpue"
+    ) |>
+    dplyr::group_by(.data$BMU, .data$landing_date) |>
+    dplyr::arrange(.by_group = T) |>
+    dplyr::ungroup()
+
+  mdb_collection_push(
+    data = summaries,
+    connection_string = conf$storage$mongodb$connection_string,
+    collection_name = conf$storage$mongod$database$dashboard$collection_name$legacy,
+    db_name = conf$storage$mongod$database$dashboard$name
+  )
+}
