0fb1821c7095e53a702ae25309cab1cfa00ab2cb
langbart <lorenzo.longobardi@gmail.com>
2026-03-03 18:59:59 +0100

    drop artifacts columns

--- R/preprocessing-surveys.R
+++ R/preprocessing-surveys.R
@@ -421,7 +421,8 @@ preprocess_kefs_surveys_v2 <- function(log_threshold = logger::DEBUG) {
         .data$habitat == "sea_grass" ~ "Seagrass",
         TRUE ~ "Multiple"
       )
-    )
+    ) |>
+    dplyr::select("airtable_id")
 
   # upload preprocessed landings
   coasts::upload_parquet_to_cloud(
