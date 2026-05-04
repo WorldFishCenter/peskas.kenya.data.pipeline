eba4554c3b2c58f45f19a0edb4f836718651eaa0
langbart <lorenzo.longobardi@gmail.com>
2026-05-04 13:46:16 +0200

    decrese total catch outlier sensitivity

--- R/validation.R
+++ R/validation.R
@@ -126,7 +126,7 @@ validate_landings <- function() {
       ),
       total_catch_alert = validate_total_catch(
         data = merged_landings,
-        k = 2.5,
+        k = 3.5,
         flag_value = 10
       ),
       fishers_catch_alert = validate_fishers_catch(
